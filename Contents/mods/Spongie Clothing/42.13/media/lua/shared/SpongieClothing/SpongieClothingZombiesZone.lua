520add122592ebef635c60d162334798dc06c922
Spongie <lazyspongie@gmail.com>
2026-01-03 00:11:53 +0000

    Update SpongieClothingZombiesZone.lua

--- Contents/mods/Spongie Clothing/42.13/media/lua/shared/SpongieClothing/SpongieClothingZombiesZone.lua
+++ Contents/mods/Spongie Clothing/42.13/media/lua/shared/SpongieClothing/SpongieClothingZombiesZone.lua
@@ -1,30 +1,37 @@
-require 'NPCs/ZombiesZoneDefinition'
 
+local defs = {
+    ["Default"] = {
+        {name="SpongieWinterGuy",chance=0.05,},
+        {name = "SpongiePoliceArmored", chance=0.05},
+        {name = "SpongieDenim", chance=4},
+        {name = "SpongieGothChick", gender="female", chance=1},
+        {name = "SpongieCoat", chance=2},
+    },
+    ["Bar"] = {
+        {name = "SpongieWinterGuy", chance=1,},
+        {name = "SpongieBikerChick", chance=2},
+    },
+    ["Police"] = {
+        {name="SpongiePoliceArmored",chance=10,},
+    },
+    ["StreetPoor"] = {
+        {name="SpongieBikerChick",chance=1,},
+    },
+    ["Rocker"] = {
+        {name="SpongieBikerChick",chance=2,},
+    },
+    ["Rich"] = {
+        {name="SpongieCoat",chance=5,},
+    },
+    ["School"] = {
+        {name = "SpongieGothChick", gender="female", chance=1},
+    },
+}
 
-table.insert(ZombiesZoneDefinition.Default,{name="SpongieWinterGuy",chance=0.05,});
-table.insert(ZombiesZoneDefinition.Bar,{name="SpongieWinterGuy",chance=1,});
-
-
---SpongiePoliceArmored
-table.insert(ZombiesZoneDefinition.Police,{name="SpongiePoliceArmored",chance=10,});
-table.insert(ZombiesZoneDefinition.Default,{name = "SpongiePoliceArmored", chance=0.2});
-
-
---SpongieBikerChick
-table.insert(ZombiesZoneDefinition.StreetPoor,{name="SpongieBikerChick",chance=1,});
-table.insert(ZombiesZoneDefinition.Rocker,{name="SpongieBikerChick",chance=2,});
-table.insert(ZombiesZoneDefinition.Default,{name = "SpongieBikerChick", chance=1});
-table.insert(ZombiesZoneDefinition.Bar,{name = "SpongieBikerChick", chance=2});
-
-
---SpongieDenim
-table.insert(ZombiesZoneDefinition.Rocker,{name="SpongieDenim",chance=10,});
-table.insert(ZombiesZoneDefinition.Default,{name = "SpongieDenim", chance=4});
-
---SpongieGothChick
-table.insert(ZombiesZoneDefinition.Default,{name = "SpongieGothChick", gender="female", chance=1});
-table.insert(ZombiesZoneDefinition.School,{name = "SpongieGothChick", gender="female", chance=1});
-
---SpongieCoat
-table.insert(ZombiesZoneDefinition.Rich,{name="SpongieCoat",chance=5,});
-table.insert(ZombiesZoneDefinition.Default,{name = "SpongieCoat", chance=2});
+for defName, list in pairs(defs) do
+    for i, outfit in ipairs(list) do
+        ZombiesZoneDefinition[defName][outfit.name] = outfit
+        -- table.insert(ZombiesZoneDefinition[defName],outfit)
+    end
+end
+defs = nil
